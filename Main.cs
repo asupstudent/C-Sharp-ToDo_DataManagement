42d9f61ab631e11e67cd2438e68654397155d678
asupstudent <asupstudent@yandex.ru>
2023-12-20 19:44:13 +0300

    Заканчивает проект

--- Main.cs
+++ Main.cs
@@ -660,16 +660,74 @@ namespace C_Sharp_ToDo_DataManagement
             }
         }
 
+        //public bool WriteDataTableToExcel(System.Data.DataTable dataTable, string worksheetName, string saveAsLocation, string ReporType)
+        //{
+        //    Microsoft.Office.Interop.Excel.Application excel;
+        //    Microsoft.Office.Interop.Excel.Workbook excelworkBook;
+        //    Microsoft.Office.Interop.Excel.Worksheet excelSheet;
+
+        //    try
+        //    {
+        //        //  get Application object.
+        //        excel = new Microsoft.Office.Interop.Excel.Application();
+        //        excel.Visible = false;
+        //        excel.DisplayAlerts = false;
+
+        //        // Creation a new Workbook
+        //        excelworkBook = excel.Workbooks.Add(Type.Missing);
+
+        //        // Workk sheet
+        //        excelSheet = (Microsoft.Office.Interop.Excel.Worksheet)excelworkBook.ActiveSheet;
+        //        excelSheet.Name = worksheetName;
+
+        //        // loop through each row and add values to our sheet
+        //        int rowcount = 0;
+
+        //        foreach (DataRow datarow in dataTable.Rows)
+        //        {
+        //            rowcount += 1;
+        //            for (int i = 1; i <= dataTable.Columns.Count; i++)
+        //            {
+        //                // on the first iteration we add the column headers
+        //                if (rowcount == 2)
+        //                {
+        //                    excelSheet.Cells[1, i] = dataTable.Columns[i - 1].ColumnName;
+        //                }
+        //                // Filling the excel file 
+        //                excelSheet.Cells[rowcount, i] = datarow[i - 1].ToString();
+        //            }
+        //        }
+        //        excelSheet.Columns.EntireColumn.AutoFit();
+        //        //now save the workbook and exit Excel
+        //        excelworkBook.SaveAs(saveAsLocation); ;
+        //        excelworkBook.Close();
+        //        excel.Quit();
+        //        return true;
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        MessageBox.Show(ex.Message);
+        //        return false;
+        //    }
+        //    finally
+        //    {
+        //        excelSheet = null;
+        //        excelworkBook = null;
+        //    }
+        //}
         public bool WriteDataTableToExcel(System.Data.DataTable dataTable, string worksheetName, string saveAsLocation, string ReporType)
         {
             Microsoft.Office.Interop.Excel.Application excel;
             Microsoft.Office.Interop.Excel.Workbook excelworkBook;
             Microsoft.Office.Interop.Excel.Worksheet excelSheet;
+            Microsoft.Office.Interop.Excel.Range excelCellrange;
 
             try
             {
-                //  get Application object.
+                // Start Excel and get Application object.
                 excel = new Microsoft.Office.Interop.Excel.Application();
+
+                // for making Excel visible
                 excel.Visible = false;
                 excel.DisplayAlerts = false;
 
@@ -680,8 +738,11 @@ namespace C_Sharp_ToDo_DataManagement
                 excelSheet = (Microsoft.Office.Interop.Excel.Worksheet)excelworkBook.ActiveSheet;
                 excelSheet.Name = worksheetName;
 
+                excelSheet.Cells[1, 1] = ReporType;
+                excelSheet.Cells[1, 2] = "Date : " + DateTime.Now.ToShortDateString();
+
                 // loop through each row and add values to our sheet
-                int rowcount = 0;
+                int rowcount = 2;
 
                 foreach (DataRow datarow in dataTable.Rows)
                 {
@@ -691,17 +752,46 @@ namespace C_Sharp_ToDo_DataManagement
                         // on the first iteration we add the column headers
                         if (rowcount == 3)
                         {
-                            excelSheet.Cells[1, i] = dataTable.Columns[i - 1].ColumnName;
+                            excelSheet.Cells[2, i] = dataTable.Columns[i - 1].ColumnName;
+                            excelSheet.Cells.Font.Color = System.Drawing.Color.Black;
                         }
-                        // Filling the excel file 
+
                         excelSheet.Cells[rowcount, i] = datarow[i - 1].ToString();
+
+                        //for alternate rows
+                        if (rowcount > 3)
+                        {
+                            if (i == dataTable.Columns.Count)
+                            {
+                                if (rowcount % 2 == 0)
+                                {
+                                    excelCellrange = excelSheet.Range[excelSheet.Cells[rowcount, 1], excelSheet.Cells[rowcount, dataTable.Columns.Count]];
+                                    FormattingExcelCells(excelCellrange, "#CCCCFF", System.Drawing.Color.Black, false);
+                                }
+
+                            }
+                        }
                     }
                 }
-                excelSheet.Columns.EntireColumn.AutoFit();
+
+                // now we resize the columns
+                excelCellrange = excelSheet.Range[excelSheet.Cells[1, 1], excelSheet.Cells[rowcount, dataTable.Columns.Count]];
+                excelCellrange.EntireColumn.AutoFit();
+                Microsoft.Office.Interop.Excel.Borders border = excelCellrange.Borders;
+                border.LineStyle = Microsoft.Office.Interop.Excel.XlLineStyle.xlContinuous;
+                border.Weight = 2d;
+
+
+                excelCellrange = excelSheet.Range[excelSheet.Cells[1, 1], excelSheet.Cells[2, dataTable.Columns.Count]];
+                FormattingExcelCells(excelCellrange, "#FF6347", System.Drawing.Color.Black, true);
+
+
                 //now save the workbook and exit Excel
+
                 excelworkBook.SaveAs(saveAsLocation); ;
                 excelworkBook.Close();
                 excel.Quit();
+
                 return true;
             }
             catch (Exception ex)
@@ -712,7 +802,26 @@ namespace C_Sharp_ToDo_DataManagement
             finally
             {
                 excelSheet = null;
+                excelCellrange = null;
                 excelworkBook = null;
+            }
+
+        }
+
+        /// <summary>
+        /// FUNCTION FOR FORMATTING EXCEL CELLS
+        /// </summary>
+        /// <param name="range"></param>
+        /// <param name="HTMLcolorCode"></param>
+        /// <param name="fontColor"></param>
+        /// <param name="IsFontbool"></param>
+        public void FormattingExcelCells(Microsoft.Office.Interop.Excel.Range range, string HTMLcolorCode, System.Drawing.Color fontColor, bool IsFontbool)
+        {
+            range.Interior.Color = System.Drawing.ColorTranslator.FromHtml(HTMLcolorCode);
+            range.Font.Color = System.Drawing.ColorTranslator.ToOle(fontColor);
+            if (IsFontbool == true)
+            {
+                range.Font.Bold = IsFontbool;
             }
         }
 
@@ -753,7 +862,7 @@ namespace C_Sharp_ToDo_DataManagement
             {
                 fbCon.Close();
             }
-            WriteDataTableToExcel(dt, "Отчет по невыполненным задачам", reportPath + "\\ExpiredTasks-" + reportDate, "");
+            WriteDataTableToExcel(dt, "Отчет по невыполненным задачам", reportPath + "\\ExpiredTasks-" + reportDate, "Отчет по невыполненным задачам");
             MessageBox.Show("Отчет создан");
         }
 
@@ -795,7 +904,7 @@ namespace C_Sharp_ToDo_DataManagement
             {
                 fbCon.Close();
             }
-            WriteDataTableToExcel(dt, "Отчет по отложенным задачам", reportPath + "\\DelayedTasks-" + reportDate, "");
+            WriteDataTableToExcel(dt, "Отчет по отложенным задачам", reportPath + "\\DelayedTasks-" + reportDate, "Отчет по отложенным задачам");
             MessageBox.Show("Отчет создан");
         }
 
